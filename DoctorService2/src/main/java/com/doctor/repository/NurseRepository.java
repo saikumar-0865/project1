b2d1b25f009a3c78e54a9ab6c57162d1a84c41cd
saikumar-0865 <67175601+saikumar-0865@users.noreply.github.com>
2023-02-03 09:59:10 +0530

    Delete DoctorService2 directory

--- DoctorService2/src/main/java/com/doctor/repository/NurseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.doctor.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.doctor.entity.NurseDetails;
-
-public interface NurseRepository extends JpaRepository<NurseDetails , Integer> {
-
-}
